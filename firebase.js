f40923eb0f0c7db11451cd41024cc8f8c4a57dc0
Mick <sigmick@hotmail.com>
2021-08-21 13:55:38 +0700

    upload api

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ admin.initializeApp({
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: 'd4d70c1231b9e5bd37bfe329ca271a04d5a28cd2',
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_id: '113245844172173305425',
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
